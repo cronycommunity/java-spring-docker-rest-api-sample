2ab0e90bf9657897433a2091b99912a6625178fc
Mete Demirtas <metedemirtas26@gmail.com>
2021-04-17 17:15:16 +0200

    first commit

---
+++ src/main/java/com/cronycommunity/javaspringdockerrestapisample/DemoController.java
@@ -0,0 +1,20 @@
+package com.cronycommunity.javaspringdockerrestapisample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String hello() {
+        return String.format("Welcome");
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
---
+++ src/main/java/com/cronycommunity/javaspringdockerrestapisample/JavaSpringDockerRestApiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.cronycommunity.javaspringdockerrestapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDockerRestApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDockerRestApiSampleApplication.class, args);
+	}
+
+}
